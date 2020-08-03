1961dc67227822e90b3aad0e702007ab0e72fb79
YonminMa <2083198401@qq.com>
2020-08-03 19:32:44 +0800

    添加错误页面处理机制

--- src/main/java/yonmin/blog/component/MyErrorAttributes.java
+++ src/main/java/yonmin/blog/component/MyErrorAttributes.java
@@ -1,4 +1,18 @@
 package yonmin.blog.component;
 
-public class MyErrorAttributes {
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Component
+public class MyErrorAttributes extends DefaultErrorAttributes {
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+        errorAttributes.put("author","yonmin");
+        return errorAttributes;
+    }
 }
