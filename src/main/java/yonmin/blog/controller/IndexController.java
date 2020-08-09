a1f8bb484523dd7c1d786c02f08e7d3b04418831
YonminMa <2083198401@qq.com>
2020-08-09 14:57:25 +0800

    完善首页功能

--- src/main/java/yonmin/blog/controller/IndexController.java
+++ src/main/java/yonmin/blog/controller/IndexController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import yonmin.blog.dao.BlogRepository;
-import yonmin.blog.domain.Blog;
 import yonmin.blog.service.BlogService;
 import yonmin.blog.service.TypeService;
 
