29bf629a8b9d07638e03fd611a058b0bda9dd25b
YonminMa <2083198401@qq.com>
2020-08-09 10:39:49 +0800

    分类展示页面功能完善

---
+++ src/main/java/yonmin/blog/controller/TypeShowController.java
@@ -0,0 +1,41 @@
+package yonmin.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import yonmin.blog.domain.Type;
+import yonmin.blog.service.BlogService;
+import yonmin.blog.service.TypeService;
+import yonmin.blog.vo.BlogQuery;
+
+import java.util.List;
+
+@Controller
+public class TypeShowController {
+
+    @Autowired
+    private TypeService typeService;
+
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping("/types/{id}")
+    public String types(@PageableDefault(size = 8, sort = {"createTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                        @PathVariable Long id, Model model) {
+        List<Type> types = typeService.listTypeTop(10000);
+        if (id == -1 && types.size() > 0) {
+            id = types.get(0).getId();
+        }
+        BlogQuery blogQuery = new BlogQuery();
+        blogQuery.setTypeId(id);
+        model.addAttribute("types", types);
+        model.addAttribute("page", blogService.listBlog(pageable, blogQuery));
+        model.addAttribute("activeTypeId", id);
+        return "/types";
+    }
+}
